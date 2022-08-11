f337a7e76e9d77d3ee8222c7e8f8787e2c5405df
rakhmatov-kamoliddin <raxmatovkamoliddin746@gmail.com>
2022-08-11 11:49:16 +0500

    'lkjhsafs'

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -32,14 +32,14 @@ async def bot_start(message: types.Message):
         db.add_user(id=message.from_user.id,
                     name=name)
     except sqlite3.IntegrityError as err:
-        await bot.send_message(chat_id=ADMINS[0], text=err)
+        await bot.send_message(chat_id=ADMINS, text=err)
 
     await message.answer("Xush kelibsiz!",reply_markup=defaultkeyboard.defaults_buttons)
     await message.answer('Menu:',reply_markup=OurInlineKeyboard.Course)
     # Adminga xabar beramiz
     count = db.count_users()[0]
     msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-    await bot.send_message(chat_id=ADMINS[0], text=msg)
+    await bot.send_message(chat_id=ADMINS, text=msg)
 
 
 

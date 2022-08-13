e9631e3deaa220602d24688d05218a8626fbec7a
rakhmatov-kamoliddin <raxmatovkamoliddin746@gmail.com>
2022-08-13 10:07:28 +0500

    'gfdsasdfghjgfds'

--- data/config.py
+++ data/config.py
@@ -2,6 +2,6 @@ import os
 
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
-ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
+ADMINS = str(os.environ.get("ADMINS")).split() # adminlar ro'yxati
 IP = str(os.environ.get("ip"))  # Xosting ip manzili
 
